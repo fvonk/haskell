7f886c6717bab50568147d35efe840bc33d094cc
Pavel Kozlov <pavelcauselov@gmail.com>
2019-10-27 23:36:44 +0300

    up

--- module1/hello.hs
+++ module1/hello.hs
@@ -1,3 +1,10 @@
 --hello.hs my first Haskell-file!
-main = do
-  putStrLn "Hello Pavel!"
+-- main = do
+check n = if n `mod` 3 == 0
+          then n - 2
+          else 3 * n + 1
+  -- calcChange owed given = if change > 0
+  --                         then change
+  --                         else 0
+  --   where
+  --     change = given - owed
