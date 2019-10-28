ff44aa2e797b015bb0f57c6c4acf9ad56b87e074
Pavel Kozlov <pavelcauselov@gmail.com>
2019-10-28 23:05:47 +0300

    update

--- module1/hello.hs
+++ module1/hello.hs
@@ -1,5 +1,20 @@
 --hello.hs my first Haskell-file!
 -- main = do
+
+doubleDouble x = dubs*2
+  where dubs = x*2
+
+doubleD x = (\dubs -> dubs*2) x*2
+
+overwrite x = let x = 2
+              in let x = 3
+              in let x = 4
+              in x
+
+overwrite2 x = (\x -> x) ((\x -> 4) ((\x -> 3) 2))
+-- overwrite3 x = (\x -> (\x -> (\x -> x) 4) 3) 2
+over x = (\_ -> (\_ -> (\x -> x) 4) 3) 2
+
 check n = if n `mod` 3 == 0
           then n - 2
           else 3 * n + 1
